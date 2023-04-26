3989c14d07ed896e0f92604315315615abae2d12
LorenzoBertarelli <lorenzobertarelli59@gmail.com>
2023-04-26 20:01:06 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppFooter from './components/AppFooter.vue';
+import AppHeader from './components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter,
+    },
+}
+</script>
+
+<template>
+  <AppHeader />
+  <AppFooter />
+</template>
+
+<style lang="scss">
+@use "./style/general.scss" as *
+</style>
